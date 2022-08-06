f0db662e11ae1401ae4f13c3a38236e8d9186a3c
cwjki <cwjki97@gmail.com>
2022-08-06 13:18:35 -0400

    adding faculties in front

---
+++ client/quasar-frontend/src/components/tables/teachingAssignments/FacultyTableHandler.vue
@@ -0,0 +1,38 @@
+<template>
+    <generic-crud-data-table :config="config" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { GenericCrudTableConfig } from '../../genericCrudTable/models/table.model';
+import GenericCrudDataTable from '../../genericCrudTable/views/GenericCrudDataTable.vue';
+import { facultyService } from 'src/services';
+
+export default defineComponent({
+    name: 'facultyHandler',
+    components: { GenericCrudDataTable },
+    props: {},
+    emits: [],
+    setup(props, { emit }) {
+        const config = ref<GenericCrudTableConfig>({
+            name: 'Facultades',
+            singularLabel: 'Facultad',
+            service: facultyService,
+            fields: [
+                {
+                    name: 'name',
+                    label: 'Nombre',
+                    type: 'text',
+                },
+            ],
+            actions: {
+                create: true,
+                update: true,
+                delete: true,
+            },
+        });
+
+        return { config };
+    },
+});
+</script>
