a6d61473be4f2bd8e4ce28077cb4a8ff67aaf1a8
cwjki <cwjki97@gmail.com>
2022-08-08 01:03:32 -0400

    work for subjects

--- server/managementSystem/teachingAssignment/management/commands/save_database.py
+++ server/managementSystem/teachingAssignment/management/commands/save_database.py
@@ -1,14 +1,35 @@
-from ast import Raise
-from django.core.management.base import BaseCommand, CommandError, CommandParser
+import csv
+import os
+from django.core.management.base import BaseCommand, CommandError
 from ...models import Subject
+from ...serializers import SubjectSerializer
+
+CURRENT_PATH = os.path.dirname(__file__)
+SUBJECTS_DIR = os.path.join(CURRENT_PATH, '../../excels/subjects.csv')
 
 
 class Command(BaseCommand):
     help = 'Save database data in excels'
 
     def handle(self, *args, **options):
-        try:
-            subjects = Subject.objects.all()
+        # try:
+        subjects = Subject.objects.all()
 
-        except:
-            raise CommandError("Something went wrong")
+        fieldnames = ['id', 'name', 'department', 'career',
+                      'study_plan', 'semester', 'number_of_hours']
+
+        data = []
+        for subject in subjects:
+            subject = SubjectSerializer(subject).data
+            data.append(subject)
+
+        with open(SUBJECTS_DIR, 'w', encoding='UTF8') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
+
+    
+
+        # except:
+        #     raise CommandError("Something went wrong")
