dcf8e7e14d20594fd12b50c40086c66cdef085ab
cwjki <cwjki97@gmail.com>
2022-08-21 00:05:09 -0400

    fixing views issues in front

--- client/quasar-frontend/src/components/tables/teachingAssignments/TeachingAssignmentTableHandler.vue
+++ client/quasar-frontend/src/components/tables/teachingAssignments/TeachingAssignmentTableHandler.vue
@@ -11,6 +11,7 @@ import {
 import { defineComponent, ref } from 'vue';
 import { GenericCrudTableConfig } from '../../genericCrudTable/models/table.model';
 import GenericCrudDataTable from '../../genericCrudTable/views/GenericCrudDataTable.vue';
+import { ListResult } from '../../../services/api.service';
 
 export default defineComponent({
     components: { GenericCrudDataTable },
@@ -18,6 +19,10 @@ export default defineComponent({
     props: {},
     emits: [],
     setup(props, { emit }) {
+        const list: ListResult<number> = {
+            results: [1, 2, 3],
+            count: 3,
+        };
         const config = ref<GenericCrudTableConfig>({
             name: 'Asignaciones de docencia',
             singularLabel: 'Asignación de docencia',
@@ -70,15 +75,15 @@ export default defineComponent({
                         },
                     },
                 },
-                {
-                    name: 'time_period',
-                    label: 'Período de tiempo',
-                    column: {
-                        transform(row) {
-                            return `${row.subject_description.time_period}`;
-                        },
-                    },
-                },
+                // {
+                //     name: 'time_period',
+                //     label: 'Período de tiempo',
+                //     column: {
+                //         transform(row) {
+                //             return `${row.subject_description.time_period}`;
+                //         },
+                //     },
+                // },
                 {
                     name: 'number_of_hours',
                     label: 'Cantidad de horas',
@@ -94,6 +99,15 @@ export default defineComponent({
                     column: {
                         transform(row) {
                             return `${row.subject_description.number_of_groups}`;
+                        },
+                    },
+                },
+                {
+                    name: 'group',
+                    label: 'Grupo',
+                    column: {
+                        transform(row) {
+                            return `${row.group}`;
                         },
                     },
                 },
@@ -114,17 +128,20 @@ export default defineComponent({
                         list: professorService.list,
                         value: 'id',
                         label: 'name',
+                        refactorValue: (value) =>
+                            value
+                                ? `${value.name + ' ' + value.last_name}`
+                                : '',
                     },
                 },
                 {
                     name: 'percent',
                     label: 'Porciento',
-                    type: 'text',
-                },
-                {
-                    name: 'group',
-                    label: 'Grupo',
-                    type: 'text',
+                    column: {
+                        transform(row) {
+                            return `${row.percent}%`;
+                        },
+                    },
                 },
             ],
             actions: {
