f6241c7963f1765caf5c53fa510bcf3b74aff475
cwjki <cwjki97@gmail.com>
2022-08-27 17:05:14 -0400

    proccessing all data correctly before create a csv file

--- server/managementSystem/teachingAssignment/management/commands/get_csv.py
+++ server/managementSystem/teachingAssignment/management/commands/get_csv.py
@@ -1,7 +1,5 @@
 import os
 import csv
-from selectors import SelectSelector
-from traceback import format_exc
 from typing import List
 from ...serializers import ProfessorSerializer, SubjectSerializer, FacultySerializer, CareerSerializer, StudyPlanSerializer, TeachingAssignmentSerializer, TeachingGroupSerializer, DepartmentSerializer, ScientificDegreeSerializer, TeachingCategorySerializer, ClassTypeSerializer, SemesterSerializer, TimePeriodSerializer, CarmenTableSerializer
 from ...models import Professor, Subject, Faculty, Career, StudyPlan, TeachingAssignment, TeachingGroup, Department, ScientificDegree, TeachingCategory, ClassType, Semester, TimePeriod, CarmenTable
@@ -48,17 +46,75 @@ class TeachingAssignmentInfo:
         self.subject_name = ''
         self.scholar_year = ''
         self.conf_professors = []
+        self.conf_groups = 0
+        self.conf_hours = 0
         self.cp_professors = []
-        self.faculty = ''
+        self.cp_groups = 0
+        self.cp_hours = 0
+        self.faculty = 'MATCOM'
         self.course_type = 'CRD'
+        self.number_of_hours = 0
+
+    def __repr__(self) -> str:
+        return (
+            f'Asignatura: {self.subject_name} \n'
+            f'Año: {self.scholar_year} \n'
+            f'Grupos: {self.conf_groups}/{self.cp_groups} \n'
+            f'Horas: {self.number_of_hours} \n'
+            f'Profesores de C: {self.conf_professors} \n'
+            f'Profesores de CP: {self.cp_professors} \n \n'
+
+        )
 
 
 class TeachingAssignmentInfoCollection:
     def __init__(self) -> None:
         self.teaching_assignments: List[TeachingAssignmentInfo] = []
 
+    def proccess_info(self, teaching_assignments: List[dict]):
+        for teaching_assignment in teaching_assignments:
+            self.analize_row(teaching_assignment)
+        self.compute_total_hours()
+
+    def compute_total_hours(self):
+        for ta in self.teaching_assignments:
+            conf_hours = ta.conf_hours / ta.conf_groups if ta.conf_groups != 0 else 0
+            cp_hours = ta.cp_hours / ta.cp_groups if ta.cp_groups != 0 else 0
+            ta.number_of_hours = conf_hours + cp_hours
+
     def analize_row(self, teaching_assignment: dict):
-        pass
+        # get data
+        subject_name = teaching_assignment['subject_description']['name']
+        scholar_year = teaching_assignment['subject_description']['scholar_year']
+        class_type = teaching_assignment['subject_description']['class_type']
+        number_of_hours = teaching_assignment['subject_description']['number_of_hours']
+        percent = teaching_assignment['percent']
+        group = teaching_assignment['group']
+        number_of_groups = teaching_assignment['subject_description']['number_of_groups']
+        professor_hours = float(percent / 100) * number_of_hours
+        professor_name = teaching_assignment['professor']['name'] + ' ' \
+            + teaching_assignment['professor']['last_name']
+
+        # check if the teaching assignments already exists
+        already_exists, ta = self.contains(teaching_assignment)
+        if not already_exists:
+            ta = TeachingAssignmentInfo()
+            self.teaching_assignments.append(ta)
+
+        # add the data to the teaching asignment
+        if class_type == 'Conferencia':
+            ta.conf_professors.append((professor_name, professor_hours, group))
+            ta.conf_groups = number_of_groups
+            ta.conf_hours += professor_hours
+        elif class_type == 'Clase Práctica':
+            ta.cp_professors.append((professor_name, professor_hours, group))
+            ta.cp_groups = number_of_groups
+            ta.cp_hours += professor_hours
+        else:
+            pass
+
+        ta.subject_name = subject_name
+        ta.scholar_year = scholar_year
 
     def contains(self, teaching_assignment: dict):
         '''
@@ -68,7 +124,11 @@ class TeachingAssignmentInfoCollection:
         for ta in self.teaching_assignments:
             if ta.subject_name == teaching_assignment['subject_description']['name'] and ta.scholar_year == teaching_assignment['subject_description']['scholar_year']:
                 return True, ta
-            return False, None
+        return False, None
+
+    def print_info(self):
+        for ta in self.teaching_assignments:
+            print(ta)
 
 
 class Command(BaseCommand):
@@ -85,7 +145,9 @@ class Command(BaseCommand):
         fieldnames = ['Facultad', 'Tipo curso', 'Año',
                       'Asignatura', 'Horas', 'G', 'Profesor']
 
-        print(data)
+        t_a_info = TeachingAssignmentInfoCollection()
+        t_a_info.proccess_info(data)
+        t_a_info.print_info()
 
         # with open(file_path, 'w', encoding='UTF8') as f:
         #     writer = csv.DictWriter(f, fieldnames=fieldnames)
