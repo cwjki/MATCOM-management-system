50e761c9a52042970c9d4d04b3b892fee7ac02a2
cwjki <cwjki97@gmail.com>
2022-08-30 19:34:06 -0400

    creating a component to download the csv file

---
+++ client/quasar-frontend/src/components/csvDownload/csvDownload.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="dropdown-menu mr-auto" aria-labelledby="navbarDropdown">
+        <button class="dropdown-item" v-on:click="downloadCsv()">
+            Asignación de Docencia CSV
+        </button>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/runtime-core';
+import { axios } from 'src/boot/axios';
+
+export default defineComponent({
+    setup(props) {
+        const downloadCsv = ({ state }) => {
+            return axios({
+                url: '',
+                method: 'GET',
+                responseType: 'blob',
+                headers: getHeaders(state.accessToken),
+            }).then((response) => {
+                console.log(response);
+                saveAs(new Blob([response.data]), 'newfile.csv');
+            });
+        };
+
+        return {
+            downloadCsv,
+        };
+    },
+});
+</script>
